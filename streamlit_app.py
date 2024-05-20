8c9d046eb84c74b957ebf96de7195af3712f9010
Komunela <109739627+Komunela@users.noreply.github.com>
2024-05-20 15:07:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.write(
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-
+pd_df = my_dataframe.to_pandas()
 name = title = st.text_input("Enter your name:")
 st.write("Your name is:", title)
 
@@ -34,7 +34,9 @@ if ingredients_list:
 
     for ingredient in ingredients_list:
         ingredients_string += ingredient + ', '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + ingredient)
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     st.write(ingredients_string)
 
